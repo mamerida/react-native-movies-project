b746d2bdf2c6f7c9e94a099dda87a586609d7a07
mamerida <mamerida2018@gmail.com>
2023-11-23 20:45:41 -0300

    add env variables

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,9 @@
 import { View, Text } from 'react-native'
+import {API_KEY} from "@env"
 import React from 'react'
 
 const HomeScreen = () => {
+    console.log("hil",API_KEY)
   return (
     <View>
       <Text>HomeScreen</Text>
